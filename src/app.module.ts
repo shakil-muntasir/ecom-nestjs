ccac98c6af93b25ca110c057ec308a5091bd677a
Muntasir Rahman <shakil_muntasir@hotmail.com>
2023-07-05 09:13:40 +0600

    dotenv support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import env from './configs/env';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      database: env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/env.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import { cleanEnv, str, port } from 'envalid';
+
+export default cleanEnv(process.env, {
+  DB_HOST: str({ default: 'localhost' }),
+  DB_PORT: port({ default: 5432 }),
+  DB_USERNAME: str({ default: 'postgres' }),
+  DB_PASSWORD: str(),
+  DB_DATABASE: str()
+});
